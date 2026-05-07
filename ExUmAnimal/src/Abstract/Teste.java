c75c6559e8135ff92cf53d1845e4e70aa968274f
Fesouzz <claimayato234@gmail.com>
2026-05-07 13:45:15 -0300

    Add files via upload

---
+++ ExUmAnimal/src/Abstract/Teste.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package Abstract;
+
+/**
+ *
+ * @author fesil
+ */
+public class Teste {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Animal Cachorro = new Cachorro();
+        Animal Gato = new Gato();
+        
+        Cachorro.fazerSom();
+        Gato.fazerSom();
+    }
+    
+}
